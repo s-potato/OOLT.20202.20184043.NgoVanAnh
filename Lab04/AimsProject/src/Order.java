9c12c403c6b3268fb4e9e6bf96da334f9c8ba45f
s-potato <s-potato09@users.noreply.github.com>
2021-03-30 22:08:19 +0700

    add Lab04

---
+++ Lab04/AimsProject/src/Order.java
@@ -0,0 +1,106 @@
+
+public class Order {
+	public static final int MAX_NUMBERS_ORDERED = 10;
+	
+	private DigitalVideoDisc[] itemOrdered = new DigitalVideoDisc[MAX_NUMBERS_ORDERED];
+	private int qtyOrdered = 0;
+	private MyDate dateOrdered;
+	public static final int MAX_LIMITED_ORDERS = 5;
+	private static int nbOrders = 0;
+
+	private Order(){
+		nbOrders++;
+		dateOrdered = new MyDate();
+	}
+
+	public static Order createOrder(){
+		if(nbOrders == MAX_LIMITED_ORDERS) {
+			System.out.println("Max orders reach!");
+			return null;
+		}
+		return new Order();
+	}
+
+	public boolean addDigitalVideoDisc(DigitalVideoDisc disc) {
+		if(qtyOrdered == MAX_NUMBERS_ORDERED) {
+			System.out.println("The order is almost full.");
+			return false;
+		}
+		itemOrdered[qtyOrdered] = disc;
+		qtyOrdered++;
+		System.out.println("The disc has been added.");
+		return true;
+	}
+
+	public boolean addDigitalVideoDisc(DigitalVideoDisc... dvdList){
+		if(qtyOrdered + dvdList.length > MAX_NUMBERS_ORDERED) {
+			System.out.println("The order is almost full.");
+			return false;
+		}
+		for (DigitalVideoDisc digitalVideoDisc : dvdList) {
+			itemOrdered[qtyOrdered] = digitalVideoDisc;
+			qtyOrdered++;
+		}
+		System.out.println("The disc has been added.");
+		return true;
+	}
+
+	public boolean addDigitalVideoDisc(DigitalVideoDisc dvd1, DigitalVideoDisc dvd2){
+		if(qtyOrdered == MAX_NUMBERS_ORDERED) {
+			System.out.println("The order is almost full.");
+			return false;
+		}
+		itemOrdered[qtyOrdered] = dvd1;
+		qtyOrdered++;
+		if(qtyOrdered == MAX_NUMBERS_ORDERED) {
+			System.out.println("The dvd " + dvd2.getTitle() + " can't not be added.");
+			return true;
+		}
+		itemOrdered[qtyOrdered] = dvd2;
+		qtyOrdered++;
+		return true;
+	}
+	
+	public boolean removeDigitalVideoDisc(DigitalVideoDisc disc) {
+		if(qtyOrdered == 0)	{
+			System.out.println("The order is emply.");
+			return false;
+		}
+		int i;
+		for (i = 0; i < qtyOrdered; i++) {
+			if (disc.getTitle().equals(itemOrdered[i].getTitle())) {
+				break;
+			}
+		}
+		if (i == qtyOrdered) {
+			System.out.println("The order does not have that disc.");
+			return false;
+		}
+		for (int j = i; j < qtyOrdered - 1; j++) {
+			itemOrdered[j] = itemOrdered[j+1];
+		}
+		qtyOrdered--;
+		System.out.println("The disc has been deleted.");
+		return true;
+	}
+	
+	public float totalCost() {
+		float total = 0;
+		for (int i = 0; i < qtyOrdered; i++) {
+			total += itemOrdered[i].getCost();
+		}
+		return total;
+	}
+
+	public void print() {
+		System.out.println("***********************Order***********************");
+		System.out.print("Date: ");
+		dateOrdered.print();
+		System.out.println("Ordered items: ");
+		for (int i = 0; i < qtyOrdered; i++) {
+			System.out.printf("%d. %-50s%-20s%-30s%5ds%10.2f$%n",i+1,itemOrdered[i].getTitle(),itemOrdered[i].getCategory(),itemOrdered[i].getDirector(),itemOrdered[i].getLength(),itemOrdered[i].getCost());
+		}
+		System.out.printf("Total cost: %.2f$%n", totalCost());
+		System.out.println("***************************************************");
+	}
+}
---
+++ Lab04/AimsProject/src/TestPassingParameter.java
@@ -0,0 +1,33 @@
+public class TestPassingParameter {
+    public static void main(String[] args) {
+        DigitalVideoDisc jungleDVD = new DigitalVideoDisc("Jungle");
+        DigitalVideoDisc cinderellaDVD = new DigitalVideoDisc("Cinderella");
+
+        swap(jungleDVD, cinderellaDVD);
+        System.out.println("jungle dvd title: " + jungleDVD.getTitle());
+        System.out.println("cinderella dvd title: " + cinderellaDVD.getTitle());
+
+        changeTitle(jungleDVD, cinderellaDVD.getTitle());
+        System.out.println("jungle dvd title: " + jungleDVD.getTitle());
+    }
+
+    public static void swap(Object o1, Object o2) {
+        DigitalVideoDisc dvd1 = (DigitalVideoDisc)o1;
+        if(dvd1 == null){
+            return;
+        }
+        DigitalVideoDisc dvd2 = (DigitalVideoDisc)o2;
+        if(dvd2 == null){
+            return;
+        }
+        String temp = dvd1.getTitle();
+        dvd1.setTitle(dvd2.getTitle());
+        dvd2.setTitle(temp);
+    }
+
+    public static void changeTitle(DigitalVideoDisc dvd, String title) {
+        String oldTitle = dvd.getTitle();
+        dvd.setTitle(title);
+        dvd = new DigitalVideoDisc(oldTitle);
+    }
+}
